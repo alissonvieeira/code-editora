7d3c70a74b8fb9e30955e2dd23bd8ad19f541069
Alisson Vieira <alisson.echo@gmail.com>
2017-06-14 00:23:49 -0300

    Mostrando erros com macro

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -4,19 +4,12 @@
     <div class="container">
         <div class="row">
             <h3>Nova categoria</h3>
-            @if($errors->any())
-                <ul class="alert alert-danger list-inline">
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
             {!! Form::open(['route' => 'categories.store', 'class' => 'form']) !!}
 
-            <div class="form-group">
-                {!! Form::label('name', 'Nome') !!}
+            <div class="form-group {{ $errors->first('name') ? 'has-error' : ''}}">
+                {!! Form::label('name', 'Nome', ['class' => 'control-label']) !!}
                 {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                {{-- $errors->first('name') --}}
+                {!! Form::error('name', $errors) !!}
             </div>
 
             <div class="form-group">
